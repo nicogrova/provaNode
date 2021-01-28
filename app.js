dab5fb8eb15809303953954be73698751579ee5d
Grova Nicolo <grova.nicolo@osdbsesto>
2021-01-28 13:42:18 +0100

    server

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var http = require("http");
 var express = require("express");
-
+var apiServer = express();
 var hostname = "127.0.0.1";
 var port = 3000;
+var nome="nodegrova";
+var path=require("path");
 
-var server = http.createServer((req, res) => {
-    console.log("req:", req);
-    res.statusCode = 200;
-    res.setHeader("content-Type", "text/plain");
-    res.end("Ciao belloccio!");
-});
-
-server.listen(port, hostname, () => {
+apiServer.listen(port, hostname, () => {
     console.log("server running at http://%s:%d/", hostname, port);
 });
+apiServer.get("/nome", (req, res)=>{
+    console.log("ho ricevuto una get su nome");
+    res.end("ciao,il nome del server è: "+ nome);
+});
+apiServer.get("/", function(req,res){
+    res.send("sei in home");
+})
+apiServer.get("/index", (req,res)=>{
+    res.sendFile(path.join(__dirname + "/index.html"));
+})
+apiServer.get("/req", (req,res)=>{
+    res.send("il nome del server è: " + req.query.nome);
+})
